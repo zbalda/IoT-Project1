7faf1b3982770eb17e69ddc78bbf53a3a1306894
Zachary Balda <zacharybalda23@gmail.com>
2018-10-06 11:48:24 -0400

    add step 2 starter file with listeners for buttons

---
+++ step2.java
@@ -0,0 +1,52 @@
+import com.pi4j.io.gpio.*;
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+
+public class Step2 {
+
+    public static void main(String args[]) throws InterruptedException {
+        System.out.println("<--Step2--> GPIO Listen Setup ... started.");
+
+        // create gpio controller
+        final GpioController gpio = GpioFactory.getInstance();
+
+        // provision gpio input pins with internal pull down resistors enabled
+        final GpioPinDigitalInput primaryButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, PinPullResistance.PULL_DOWN);
+        final GpioPinDigitalInput toggleButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_03, PinPullResistance.PULL_DOWN);
+
+        // set shutdown states for button input pins
+        primaryButton.setShutdownOptions(true);
+        toggleButton.setShutdownOptions(true);
+
+        // create and register primary button gpio pin listener
+        primaryButton.addListener(new GpioPinListenerDigital() {
+            @Override
+            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+                // display pin state on console
+                System.out.println(" --> PRIMARY BUTTON GPIO PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
+            }
+
+        });
+
+        // create and register toggle button gpio pin listener
+        toggleButton.addListener(new GpioPinListenerDigital() {
+            @Override
+            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+                // display pin state on console
+                System.out.println(" --> TOGGLE BUTTON GPIO PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
+            }
+
+        });
+
+        System.out.println(" ... See the listener feedback here in the console:");
+
+        // keep program running until user aborts (CTRL-C)
+        while(true) {
+            Thread.sleep(500);
+        }
+
+        // stop all GPIO activity/threads by shutting down the GPIO controller
+        // (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
+        // gpio.shutdown();   <--- implement this method call if you wish to terminate the Pi4J GPIO controller
+    }
+}s
