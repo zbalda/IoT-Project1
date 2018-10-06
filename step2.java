9b9f2a7eea99887dee7d2f4437d9aba6b6149205
Zachary Balda <zacharybalda23@gmail.com>
2018-10-06 12:10:25 -0400

    add led vars

--- step2.java
+++ step2.java
@@ -1,4 +1,9 @@
 import com.pi4j.io.gpio.*;
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
 import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 
@@ -17,6 +22,18 @@ public class Step2 {
         // set shutdown states for button input pins
         primaryButton.setShutdownOptions(true);
         toggleButton.setShutdownOptions(true);
+
+        // provision gpio output pins and turn on
+        final GpioPinDigitalOutput LED_01 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "LED_01", PinState.HIGH);
+        final GpioPinDigitalOutput LED_02_L1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "LED_02_L1", PinState.HIGH);
+        final GpioPinDigitalOutput LED_02_L1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_06, "LED_02_L2", PinState.LOW);
+        final GpioPinDigitalOutput LED_02_L1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_07, "LED_02_L3", PinState.LOW);
+
+        // set shutdown states for LED pins
+        LED_01.setShutdownOptions(true, PinState.LOW);
+        LED_02_L1.setShutdownOptions(true, PinState.LOW);
+        LED_02_L2.setShutdownOptions(true, PinState.LOW);
+        LED_02_L3.setShutdownOptions(true, PinState.LOW);
 
         // create and register primary button gpio pin listener
         primaryButton.addListener(new GpioPinListenerDigital() {
